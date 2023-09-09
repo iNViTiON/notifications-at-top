3a55cf2528d8f854df60e0a7b996295731a02883
iNViTiON <13241662+iNViTiON@users.noreply.github.com>
2023-09-09 12:42:16 +0700

    ⚡️ perf: simplified app
    - leave only one type (top-right)
    - find window only once
    - move window only when nescessary

--- topnotify.cs
+++ topnotify.cs
@@ -4,102 +4,51 @@ using System.Runtime.InteropServices;
 using System.Drawing;
 using System.Threading;
 
-public class Program {
+public class Program
+{
 
-    #region Native Stuff
-    [DllImport("user32.dll", SetLastError = true)]
-    static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+  #region Native Stuff
+  [DllImport("user32.dll", SetLastError = true)]
+  static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
-    [DllImport("user32.dll", SetLastError = true)]
-    public static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr hWndChildAfter, string className,  string windowTitle);
+  [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+  public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
 
-    [DllImport("user32.dll")]
-    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+  [DllImport("user32.dll")]
+  public static extern bool GetWindowRect(IntPtr hwnd, ref Rectangle rectangle);
 
-    [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-    public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+  const short SWP_NOSIZE = 1;
+  const short SWP_NOZORDER = 0X4;
+  const int SWP_SHOWWINDOW = 0x0040;
 
-    [DllImport("user32.dll")]
-    public static extern bool GetWindowRect(IntPtr hwnd, ref Rectangle rectangle);
+  #endregion
 
-    const short SWP_NOMOVE = 0X2;
-    const short SWP_NOSIZE = 1;
-    const short SWP_NOZORDER = 0X4;
-    const int SWP_SHOWWINDOW = 0x0040;
+  public static IntPtr FindNotificationWindow()
+  {
+    return FindWindow("Windows.UI.Core.CoreWindow", "New notification");
+  }
 
-    #endregion
-
-    public static int Cooldown; // Since The Teams Notification Stutters, Add A Cooldown To Dismiss
-
-    public static void Main(string[] args) {
-
-        while (true) {
-
-            //MS Teams Notifications
-            try{
-                var teamsHwnd = FindWindow("Chrome_WidgetWin_1", "Microsoft Teams Notification");
-                var chromeHwnd = FindWindowEx(teamsHwnd, IntPtr.Zero, "Chrome_RenderWidgetHostHWND", "Chrome Legacy Window"); // I Think MS Is Using Chrome Webview For Notifications
-
-                //If A Notification Is Showing Up
-                if (chromeHwnd != IntPtr.Zero){
-                    Cooldown = 0;
-
-                    if (System.AppDomain.CurrentDomain.FriendlyName == "topleft.exe"){
-                        //Sets to top left
-                        SetWindowPos(teamsHwnd, 0, 15, 15, 100, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-                    }
-                    else if (System.AppDomain.CurrentDomain.FriendlyName == "topright.exe"){
-                        //Sets to top right
-                        
-                        
-                        //Get the current position of the notification window
-                        Rectangle NotifyRect = new Rectangle();
-                        GetWindowRect(teamsHwnd, ref NotifyRect);
-                        
-                        NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
-                        NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
-
-                        SetWindowPos(teamsHwnd, 0, Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width - 15, 15, 100, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-                    }
-                }
-                else {
-                    if (Cooldown >= 30){
-                        SetWindowPos(teamsHwnd, 0, 0, -9999, -9999, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW); // Move To Off Screen
-                        Cooldown = 0;
-                    }
-                    Cooldown += 1; // Don't Dismiss Until 30 Frames After Signal, Prevents Stutters
-                }
-            }
-            catch{
-                //User Doesn't Have Teams
-            }
-
-            //Windows System Notifications
-            var hwnd = FindWindow("Windows.UI.Core.CoreWindow", "New notification");
-            if (System.AppDomain.CurrentDomain.FriendlyName == "topleft.exe"){
-                //Sets to top left (easy peasy)
-                //50PX Y offset to make the spacing even
-                SetWindowPos(hwnd, 0, 0, -50, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-            }
-            else if (System.AppDomain.CurrentDomain.FriendlyName == "topright.exe"){
-                //Sets to top right (not as easy)
-
-                //Get the current position of the notification window
-                Rectangle NotifyRect = new Rectangle();
-                GetWindowRect(hwnd, ref NotifyRect);
-                
-                NotifyRect.Width = NotifyRect.Width - NotifyRect.X;
-			    NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
-
-                //50PX Y offset to make the spacing even
-                SetWindowPos(hwnd, 0, Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width, -50, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
-            }
-
-            Thread.Sleep(10);
-        }
-
-        
-        Console.ReadLine();
-
+  public static IntPtr getNotificationWindow()
+  {
+    IntPtr hwnd = FindNotificationWindow();
+    while (hwnd == IntPtr.Zero)
+    {
+      hwnd = FindNotificationWindow();
+      Thread.Sleep(10);
     }
+    return hwnd;
+  }
+
+  public static void Main(string[] args)
+  {
+    IntPtr hwnd = getNotificationWindow();
+    Rectangle NotifyRect = new Rectangle();
+    while (true)
+    {
+      GetWindowRect(hwnd, ref NotifyRect);
+      if (NotifyRect.Height != NotifyRect.Y && NotifyRect.Y != -50)
+        SetWindowPos(hwnd, 0, Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width + NotifyRect.X, -50, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+      Thread.Sleep(10);
+    }
+  }
 }
